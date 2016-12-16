0b1e20035958e5de2c306de2c676ba0c5fd56c1b
René Hansen <renehh@gmail.com>
2016-12-16 16:38:15 +0100

    Updates 11p1
    Adds attempt at dynamic programming approach, marshalling current floors
    layout into a single integer and then subsequently skipping queue
    additions for layouts with marshalling values equal to any previously
    encountered layouts of same value.
    
    Still no termination, or runtime complexity too large. Currently using a
    linked list to lookup marshalled values, since the number of different
    layouts possible is too big.

--- 2016/11p1.c
+++ 2016/11p1.c
@@ -6,6 +6,7 @@
 
 #define FLOORS 4
 #define NRTGS 10
+#define ALLON4TH 1023
 
 typedef enum { POG = 1, THG, PRG, RUG, COG, POM, THM, PRM, RUM, COM } rtg;
 
@@ -17,9 +18,46 @@ typedef struct {
 void print_floors(rtg *f) {
   for (int i = 0; i < FLOORS; i++)
   {
+    printf("F%d ", i + 1);
     for (int j = 0; j < NRTGS; j++) {
-      printf("%d", *((rtg *)f + i * NRTGS + j));
-      if (j < 9) printf(", ");
+      rtg r = *((rtg *)f + i * NRTGS + j);
+      switch(r) {
+        case POG:
+          printf("POG");
+          break;
+        case THG:
+          printf("THG");
+          break;
+        case PRG:
+          printf("PRG");
+          break;
+        case RUG:
+          printf("RUG");
+          break;
+        case COG:
+          printf("COG");
+          break;
+        case POM:
+          printf("POM");
+          break;
+        case THM:
+          printf("THM");
+          break;
+        case PRM:
+          printf("PRM");
+          break;
+        case RUM:
+          printf("RUM");
+          break;
+        case COM:
+          printf("COM");
+          break;
+        default:
+          printf("  .");
+          break;
+      }
+      
+      printf(" ");
     }
     printf("\n");
   }
@@ -68,9 +106,40 @@ rtg *copy_floors(rtg *f) {
   return copy;
 }
 
+/**
+ * Since rtgs are only moved vertical in their own column, each floor
+ * constellation can be represented in 10 bits, either the rtg is present in
+ * its designated location on the floor or it isn't.
+ *
+ * Uniquely identifying the aggregate constellation of all four floors thus
+ * requires 40 bits. Assuming a long is 64 bits, all possible constellations of
+ * rtgs across floors can then each be expressed by a single 64 bit integer.
+ *
+ * Largest possible value is then:
+ *
+ *   1111111111000000000000000000000000000000 (base 2)
+ *   1098437885952                            (base 10)
+ */
+long marshall(rtg *f) {
+  long mask = 0, r;
+
+  for (int i = 0; i < FLOORS; i++)
+  {
+    for (int j = 0; j < NRTGS; j++)
+    {
+      r = *((rtg *)f + i * NRTGS + j);
+      mask |= (((long)!!r) << ((FLOORS * NRTGS - 1) - (i * NRTGS + j)));
+    }
+  }
+
+  return mask;
+}
+
 void copy_and_enqueue(ll_t *q, rtg *floors, int steps) {
-  printf("copying and enq. with steps: %d\n", steps);
+  /*printf("copying and enq. with steps: %d\n", steps);*/
   ll_node_t *n = ll_node_create();
+
+  /*print_floors(floors);*/
 
   fchecker_t *c = malloc(sizeof(fchecker_t));
   c->steps = steps;
@@ -80,11 +149,19 @@ void copy_and_enqueue(ll_t *q, rtg *floors, int steps) {
   ll_append(q, n);
 }
 
+int checked_finder(void *v0, void *v1) {
+  return *(long *)v0 == *(long *)v1;
+}
+
+void checked_printer(void *p) {
+  printf("%lu", *(long *)p);
+}
+
 int main(int argc, char const *argv[])
 {
   int steps = 0, i, j, k;
   int all_sum = POG + THG + PRG + RUG + COG + POM + THM + PRM + RUM + COM;
-
+  long *m;
 
   rtg floors[FLOORS][NRTGS] = {
     { POG, THG, THM, PRG, RUG, RUM, COG, COM, 0, 0 },
@@ -94,17 +171,29 @@ int main(int argc, char const *argv[])
   };
 
   ll_t *q = ll_list_create();
+  ll_t *checked = ll_list_create();
 
-  copy_and_enqueue(q, floors, 0);
+  copy_and_enqueue(q, (rtg *)floors, 0);
+
+  ll_node_t *cnode = ll_node_create();
+  m = malloc(sizeof(long));
+  *m = marshall((rtg *)floors);
+  cnode->value = m;
+  ll_append(checked, cnode);
 
   while (!ll_empty(q)) {
     ll_node_t *current_node = ll_remove_head(q);
     fchecker_t *current_checker = (fchecker_t *)current_node->value;
     rtg *from, *to;
 
-    for (i = 0; i < FLOORS; i++) {
-      int rtg_sum = 0;
+    // when all rtgs arrive at fourth floor, save steps if it's a new low
+    if (marshall(current_checker->floors) == ALLON4TH &&
+        current_checker->steps < steps) {
+      steps = current_checker->steps;
+      break;
+    }
 
+    for (i = 0; i < FLOORS; i++) {
       for (j = 0; j < NRTGS; j++) {
         rtg *from = (rtg *)current_checker->floors + i * NRTGS + j;
 
@@ -117,7 +206,14 @@ int main(int argc, char const *argv[])
           to = (rtg *)current_checker->floors + (i + 1) * NRTGS + j;
           swap(from, to);
 
-          if (valid(current_checker->floors)) {
+          m = malloc(sizeof(long));
+          *m = marshall((rtg *)current_checker->floors);
+
+          if (valid(current_checker->floors) &&
+                ll_find(checked, m, checked_finder) == NULL) {
+            cnode = ll_node_create();
+            cnode->value = m;
+            ll_append(checked, cnode);
             copy_and_enqueue(q, current_checker->floors, current_checker->steps + 1);
           }
 
@@ -130,7 +226,14 @@ int main(int argc, char const *argv[])
             rtg *inner_to = (rtg *)current_checker->floors + (i + 1) * NRTGS + k;
             swap(inner_from, inner_to);
 
-            if (valid(current_checker->floors)) {
+            m = malloc(sizeof(long));
+            *m = marshall((rtg *)current_checker->floors);
+
+            if (valid(current_checker->floors) &&
+                ll_find(checked, m, checked_finder) == NULL) {
+              cnode = ll_node_create();
+              cnode->value = m;
+              ll_append(checked, cnode);
               copy_and_enqueue(q, current_checker->floors, current_checker->steps + 1);
             }
 
@@ -145,19 +248,20 @@ int main(int argc, char const *argv[])
           
           swap(from, to);
 
-          if (valid(current_checker->floors)) {
+          m = malloc(sizeof(long));
+          *m = marshall((rtg *)current_checker->floors);
+
+          if (valid(current_checker->floors) &&
+              ll_find(checked, m, checked_finder) == NULL) {
+            cnode = ll_node_create();
+            cnode->value = m;
+            ll_append(checked, cnode);
             copy_and_enqueue(q, current_checker->floors, current_checker->steps + 1);
           }
 
           swap(from, to);
         }
-
-        if (i == FLOORS - 1) rtg_sum += *from;
       }
-
-      // when all rtgs arrive at fourth floor, save steps if it's a new low
-      if (i == FLOORS - 1 && rtg_sum == all_sum && current_checker->steps < steps)
-        steps = current_checker->steps;
     }
   }
 
