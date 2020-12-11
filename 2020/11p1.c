806956b66cc0e8574a449808ecf060d960dce400
René Hansen <renehh@gmail.com>
2020-12-11 22:40:24 +0100

    Adds solution to 2020 day 11 part 1

---
+++ 2020/11p1.c
@@ -0,0 +1,185 @@
+/**
+ * --- Day 11: Seating System ---
+ *
+ * Your plane lands with plenty of time to spare. The final leg of your journey
+ * is a ferry that goes directly to the tropical island where you can finally
+ * start your vacation. As you reach the waiting area to board the ferry, you
+ * realize you're so early, nobody else has even arrived yet!
+ *
+ * By modeling the process people use to choose (or abandon) their seat in the
+ * waiting area, you're pretty sure you can predict the best place to sit. You
+ * make a quick map of the seat layout (your puzzle input).
+ *
+ * The seat layout fits neatly on a grid. Each position is either floor (.), an
+ * empty seat (L), or an occupied seat (#). For example, the initial seat layout
+ * might look like this:
+ *
+ * L.LL.LL.LL
+ * LLLLLLL.LL
+ * L.L.L..L..
+ * LLLL.LL.LL
+ * L.LL.LL.LL
+ * L.LLLLL.LL
+ * ..L.L.....
+ * LLLLLLLLLL
+ * L.LLLLLL.L
+ * L.LLLLL.LL
+ *
+ * Now, you just need to model the people who will be arriving shortly.
+ * Fortunately, people are entirely predictable and always follow a simple set
+ * of rules. All decisions are based on the number of occupied seats adjacent to
+ * a given seat (one of the eight positions immediately up, down, left, right,
+ * or diagonal from the seat). The following rules are applied to every seat
+ * simultaneously:
+ *
+ * - If a seat is empty (L) and there are no occupied seats adjacent to it, the
+ *   seat becomes occupied.
+ * - If a seat is occupied (#) and four or more seats adjacent to it are also
+ *   occupied, the seat becomes empty.
+ * - Otherwise, the seat's state does not change.
+ *
+ * Floor (.) never changes; seats don't move, and nobody sits on the floor.
+ *
+ * After one round of these rules, every seat in the example layout becomes
+ * occupied:
+ *
+ * #.##.##.##
+ * #######.##
+ * #.#.#..#..
+ * ####.##.##
+ * #.##.##.##
+ * #.#####.##
+ * ..#.#.....
+ * ##########
+ * #.######.#
+ * #.#####.##
+ *
+ * After a second round, the seats with four or more occupied adjacent seats
+ * become empty again:
+ *
+ * #.LL.L#.##
+ * #LLLLLL.L#
+ * L.L.L..L..
+ * #LLL.LL.L#
+ * #.LL.LL.LL
+ * #.LLLL#.##
+ * ..L.L.....
+ * #LLLLLLLL#
+ * #.LLLLLL.L
+ * #.#LLLL.##
+ *
+ * This process continues for three more rounds:
+ *
+ * #.##.L#.##
+ * #L###LL.L#
+ * L.#.#..#..
+ * #L##.##.L#
+ * #.##.LL.LL
+ * #.###L#.##
+ * ..#.#.....
+ * #L######L#
+ * #.LL###L.L
+ * #.#L###.##
+ * #.#L.L#.##
+ * #LLL#LL.L#
+ * L.L.L..#..
+ * #LLL.##.L#
+ * #.LL.LL.LL
+ * #.LL#L#.##
+ * ..L.L.....
+ * #L#LLLL#L#
+ * #.LLLLLL.L
+ * #.#L#L#.##
+ * #.#L.L#.##
+ * #LLL#LL.L#
+ * L.#.L..#..
+ * #L##.##.L#
+ * #.#L.LL.LL
+ * #.#L#L#.##
+ * ..L.L.....
+ * #L#L##L#L#
+ * #.LLLLLL.L
+ * #.#L#L#.##
+ *
+ * At this point, something interesting happens: the chaos stabilizes and
+ * further applications of these rules cause no seats to change state! Once
+ * people stop moving around, you count 37 occupied seats.
+ *
+ * Simulate your seating area by applying the seating rules repeatedly until no
+ * seats change state. How many seats end up occupied?
+*/
+
+#include <stdio.h>
+
+#define INPUT_SIZE 90
+#define LINE_LENGTH 99
+
+char layout[INPUT_SIZE][LINE_LENGTH + 1];
+int adjecents[INPUT_SIZE][LINE_LENGTH + 1];
+
+int count_adjecents(int i, int j) {
+  int k, l, c = 0;
+
+  for(k = i - 1; k <= i + 1; k++) {
+    if (k < 0 || k == INPUT_SIZE) continue;
+
+    for(l = j - 1; l <= j + 1; l++) {
+      if (l < 0 || l == LINE_LENGTH) continue;
+      if (k == i && l == j) continue;
+
+      c += layout[k][l] == '#';
+    }
+  }
+
+  return c;
+}
+
+void fill_adjecents() {
+  for(int i = 0; i < INPUT_SIZE; i++) {
+    for(int j = 0; j < LINE_LENGTH; j++) {
+      adjecents[i][j] = count_adjecents(i, j);
+    }
+  }
+}
+
+int occupancy() {
+  int count = 0;
+
+  for(int i = 0; i < INPUT_SIZE; i++) {
+    for(int j = 0; j < LINE_LENGTH; j++) {
+      count += layout[i][j] == '#';
+    }
+  }
+
+  return count;
+}
+
+int main(int argc, char **argv) {
+  int i = 0, j, changes;
+
+  while(scanf("%99s\n", layout[i++]) != EOF) {}
+
+  do {
+    changes = 0;
+    fill_adjecents();
+
+    for(i = 0; i < INPUT_SIZE; i++) {
+      for(j = 0; j < LINE_LENGTH; j++) {
+        int empty = layout[i][j] == 'L';
+        int occupied = layout[i][j] == '#';
+
+        if (empty && adjecents[i][j] == 0) {
+          layout[i][j] = '#';
+          changes = 1;
+        } else if (occupied && adjecents[i][j] >= 4) {
+          layout[i][j] = 'L';
+          changes = 1;
+        }
+      }
+    }
+  } while (changes);
+
+  printf("Number of seats ending up occupied: %d\n", occupancy());
+
+  return 0;
+}
