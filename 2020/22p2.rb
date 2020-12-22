587865e5fdb0e10bb75f4734b391985fb6521d3f
René Hansen <renehh@gmail.com>
2020-12-22 19:11:58 +0100

    Updates solution to 2020 part 2
    It's no longer too slow.

--- 2020/22p2.rb
+++ 2020/22p2.rb
@@ -1,113 +1,432 @@
-top, bottom = STDIN.read.split("\n\n")
+=begin
+
+--- Part Two ---
+
+You lost to the small crab! Fortunately, crabs aren't very good at recursion. To
+defend your honor as a Raft Captain, you challenge the small crab to a game of
+Recursive Combat.
+
+Recursive Combat still starts by splitting the cards into two decks (you offer
+to play with the same starting decks as before - it's only fair). Then, the game
+consists of a series of rounds with a few changes:
+
+- Before either player deals a card, if there was a previous round in this game
+that had exactly the same cards in the same order in the same players' decks,
+the game instantly ends in a win for player 1. Previous rounds from other games
+are not considered. (This prevents infinite games of Recursive Combat, which
+everyone agrees is a bad idea.)
+- Otherwise, this round's cards must be in a new configuration; the players
+begin the round by each drawing the top card of their deck as normal.
+- If both players have at least as many cards remaining in their deck as the
+value of the card they just drew, the winner of the round is determined by
+playing a new game of Recursive Combat (see below).
+- Otherwise, at least one player must not have enough cards left in their deck
+to recurse; the winner of the round is the player with the higher-value card.
+
+As in regular Combat, the winner of the round (even if they won the round by
+winning a sub-game) takes the two cards dealt at the beginning of the round and
+places them on the bottom of their own deck (again so that the winner's card is
+above the other card). Note that the winner's card might be the lower-valued of
+the two cards if they won the round due to winning a sub-game. If collecting
+cards by winning the round causes a player to have all of the cards, they win,
+and the game ends.
+
+Here is an example of a small game that would loop forever without the infinite
+game prevention rule:
+
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+
+During a round of Recursive Combat, if both players have at least as many cards
+in their own decks as the number on the card they just dealt, the winner of the
+round is determined by recursing into a sub-game of Recursive Combat. (For
+example, if player 1 draws the 3 card, and player 2 draws the 7 card, this would
+occur if player 1 has at least 3 cards left and player 2 has at least 7 cards
+left, not counting the 3 and 7 cards that were drawn.)
+
+To play a sub-game of Recursive Combat, each player creates a new deck by making
+a copy of the next cards in their deck (the quantity of cards copied is equal to
+the number on the card they drew to trigger the sub-game). During this sub-game,
+the game that triggered it is on hold and completely unaffected; no cards are
+removed from players' decks to form the sub-game. (For example, if player 1 drew
+the 3 card, their deck in the sub-game would be copies of the next three cards
+in their deck.)
+
+Here is a complete example of gameplay, where Game 1 is the primary game of
+Recursive Combat:
+
+=== Game 1 ===
+
+-- Round 1 (Game 1) --
+Player 1's deck: 9, 2, 6, 3, 1
+Player 2's deck: 5, 8, 4, 7, 10
+Player 1 plays: 9
+Player 2 plays: 5
+Player 1 wins round 1 of game 1!
+
+-- Round 2 (Game 1) --
+Player 1's deck: 2, 6, 3, 1, 9, 5
+Player 2's deck: 8, 4, 7, 10
+Player 1 plays: 2
+Player 2 plays: 8
+Player 2 wins round 2 of game 1!
+
+-- Round 3 (Game 1) --
+Player 1's deck: 6, 3, 1, 9, 5
+Player 2's deck: 4, 7, 10, 8, 2
+Player 1 plays: 6
+Player 2 plays: 4
+Player 1 wins round 3 of game 1!
+
+-- Round 4 (Game 1) --
+Player 1's deck: 3, 1, 9, 5, 6, 4
+Player 2's deck: 7, 10, 8, 2
+Player 1 plays: 3
+Player 2 plays: 7
+Player 2 wins round 4 of game 1!
+
+-- Round 5 (Game 1) --
+Player 1's deck: 1, 9, 5, 6, 4
+Player 2's deck: 10, 8, 2, 7, 3
+Player 1 plays: 1
+Player 2 plays: 10
+Player 2 wins round 5 of game 1!
+
+-- Round 6 (Game 1) --
+Player 1's deck: 9, 5, 6, 4
+Player 2's deck: 8, 2, 7, 3, 10, 1
+Player 1 plays: 9
+Player 2 plays: 8
+Player 1 wins round 6 of game 1!
+
+-- Round 7 (Game 1) --
+Player 1's deck: 5, 6, 4, 9, 8
+Player 2's deck: 2, 7, 3, 10, 1
+Player 1 plays: 5
+Player 2 plays: 2
+Player 1 wins round 7 of game 1!
+
+-- Round 8 (Game 1) --
+Player 1's deck: 6, 4, 9, 8, 5, 2
+Player 2's deck: 7, 3, 10, 1
+Player 1 plays: 6
+Player 2 plays: 7
+Player 2 wins round 8 of game 1!
+
+-- Round 9 (Game 1) --
+Player 1's deck: 4, 9, 8, 5, 2
+Player 2's deck: 3, 10, 1, 7, 6
+Player 1 plays: 4
+Player 2 plays: 3
+Playing a sub-game to determine the winner...
+
+=== Game 2 ===
+
+-- Round 1 (Game 2) --
+Player 1's deck: 9, 8, 5, 2
+Player 2's deck: 10, 1, 7
+Player 1 plays: 9
+Player 2 plays: 10
+Player 2 wins round 1 of game 2!
+
+-- Round 2 (Game 2) --
+Player 1's deck: 8, 5, 2
+Player 2's deck: 1, 7, 10, 9
+Player 1 plays: 8
+Player 2 plays: 1
+Player 1 wins round 2 of game 2!
+
+-- Round 3 (Game 2) --
+Player 1's deck: 5, 2, 8, 1
+Player 2's deck: 7, 10, 9
+Player 1 plays: 5
+Player 2 plays: 7
+Player 2 wins round 3 of game 2!
+
+-- Round 4 (Game 2) --
+Player 1's deck: 2, 8, 1
+Player 2's deck: 10, 9, 7, 5
+Player 1 plays: 2
+Player 2 plays: 10
+Player 2 wins round 4 of game 2!
+
+-- Round 5 (Game 2) --
+Player 1's deck: 8, 1
+Player 2's deck: 9, 7, 5, 10, 2
+Player 1 plays: 8
+Player 2 plays: 9
+Player 2 wins round 5 of game 2!
+
+-- Round 6 (Game 2) --
+Player 1's deck: 1
+Player 2's deck: 7, 5, 10, 2, 9, 8
+Player 1 plays: 1
+Player 2 plays: 7
+Player 2 wins round 6 of game 2!
+The winner of game 2 is player 2!
+
+...anyway, back to game 1.
+Player 2 wins round 9 of game 1!
+
+-- Round 10 (Game 1) --
+Player 1's deck: 9, 8, 5, 2
+Player 2's deck: 10, 1, 7, 6, 3, 4
+Player 1 plays: 9
+Player 2 plays: 10
+Player 2 wins round 10 of game 1!
+
+-- Round 11 (Game 1) --
+Player 1's deck: 8, 5, 2
+Player 2's deck: 1, 7, 6, 3, 4, 10, 9
+Player 1 plays: 8
+Player 2 plays: 1
+Player 1 wins round 11 of game 1!
+
+-- Round 12 (Game 1) --
+Player 1's deck: 5, 2, 8, 1
+Player 2's deck: 7, 6, 3, 4, 10, 9
+Player 1 plays: 5
+Player 2 plays: 7
+Player 2 wins round 12 of game 1!
+
+-- Round 13 (Game 1) --
+Player 1's deck: 2, 8, 1
+Player 2's deck: 6, 3, 4, 10, 9, 7, 5
+Player 1 plays: 2
+Player 2 plays: 6
+Playing a sub-game to determine the winner...
+
+=== Game 3 ===
+
+-- Round 1 (Game 3) --
+Player 1's deck: 8, 1
+Player 2's deck: 3, 4, 10, 9, 7, 5
+Player 1 plays: 8
+Player 2 plays: 3
+Player 1 wins round 1 of game 3!
+
+-- Round 2 (Game 3) --
+Player 1's deck: 1, 8, 3
+Player 2's deck: 4, 10, 9, 7, 5
+Player 1 plays: 1
+Player 2 plays: 4
+Playing a sub-game to determine the winner...
+
+=== Game 4 ===
+
+-- Round 1 (Game 4) --
+Player 1's deck: 8
+Player 2's deck: 10, 9, 7, 5
+Player 1 plays: 8
+Player 2 plays: 10
+Player 2 wins round 1 of game 4!
+The winner of game 4 is player 2!
+
+...anyway, back to game 3.
+Player 2 wins round 2 of game 3!
+
+-- Round 3 (Game 3) --
+Player 1's deck: 8, 3
+Player 2's deck: 10, 9, 7, 5, 4, 1
+Player 1 plays: 8
+Player 2 plays: 10
+Player 2 wins round 3 of game 3!
+
+-- Round 4 (Game 3) --
+Player 1's deck: 3
+Player 2's deck: 9, 7, 5, 4, 1, 10, 8
+Player 1 plays: 3
+Player 2 plays: 9
+Player 2 wins round 4 of game 3!
+The winner of game 3 is player 2!
+
+...anyway, back to game 1.
+Player 2 wins round 13 of game 1!
+
+-- Round 14 (Game 1) --
+Player 1's deck: 8, 1
+Player 2's deck: 3, 4, 10, 9, 7, 5, 6, 2
+Player 1 plays: 8
+Player 2 plays: 3
+Player 1 wins round 14 of game 1!
+
+-- Round 15 (Game 1) --
+Player 1's deck: 1, 8, 3
+Player 2's deck: 4, 10, 9, 7, 5, 6, 2
+Player 1 plays: 1
+Player 2 plays: 4
+Playing a sub-game to determine the winner...
+
+=== Game 5 ===
+
+-- Round 1 (Game 5) --
+Player 1's deck: 8
+Player 2's deck: 10, 9, 7, 5
+Player 1 plays: 8
+Player 2 plays: 10
+Player 2 wins round 1 of game 5!
+The winner of game 5 is player 2!
+
+...anyway, back to game 1.
+Player 2 wins round 15 of game 1!
+
+-- Round 16 (Game 1) --
+Player 1's deck: 8, 3
+Player 2's deck: 10, 9, 7, 5, 6, 2, 4, 1
+Player 1 plays: 8
+Player 2 plays: 10
+Player 2 wins round 16 of game 1!
+
+-- Round 17 (Game 1) --
+Player 1's deck: 3
+Player 2's deck: 9, 7, 5, 6, 2, 4, 1, 10, 8
+Player 1 plays: 3
+Player 2 plays: 9
+Player 2 wins round 17 of game 1!
+The winner of game 1 is player 2!
+
+
+== Post-game results ==
+Player 1's deck:
+Player 2's deck: 7, 5, 6, 2, 4, 1, 10, 8, 9, 3
+
+After the game, the winning player's score is calculated from the cards they
+have in their original deck using the same rules as regular Combat. In the above
+game, the winning player's score is 291.
+
+Defend your honor as Raft Captain by playing the small crab in a game of
+Recursive Combat using the same two decks as before. What is the winning
+player's score?
+
+=end
 
 DEBUG = false
 
-def dputs(*args)
+class Deck
+  attr_accessor :cards
+
+  def initialize(cards=[])
+    @cards = cards
+  end
+
+  def to_s
+    cards.join(", ")
+  end
+end
+
+class Player
+  attr_accessor :deck
+  attr_accessor :no
+
+  def initialize(cards=[], no)
+    @deck = Deck.new(cards)
+    @no = no
+  end
+
+  def cards
+    deck.cards
+  end
+
+  def with_new_cards(count)
+    Player.new(deck.cards[0, count], no)
+  end
+
+  def pick
+    deck.cards.shift
+  end
+
+  def to_s
+    no
+  end
+
+  def ==(other)
+    no == other.no
+  end
+end
+
+def debug(*args)
   puts *args if DEBUG
 end
+
+top, bottom = STDIN.read.split("\n\n")
 
 p1_cards = top.split("\n")[1..-1].map(&:to_i)
 p2_cards = bottom.split("\n")[1..-1].map(&:to_i)
 
-ggame = 0
+p1 = Player.new(p1_cards, "1")
+p2 = Player.new(p2_cards, "2")
+game = 0
 
-igp_rule = lambda do |hand_history|
-  not hand_history.detect do |round|
-    hand_history.count(round) > 1
-  end.nil?
-end
-
-calc_score = lambda do |hand|
-  score = 0
-  hand.each_with_index do |card,i|
-    score += card * (hand.length - i)
-  end
-  score
-end
-
-@memo = Hash.new { |h,k| h[k] = Hash.new }
-
-play = lambda do |p1c, p2c|
-  memo = @memo[p1c][p2c] || @memo[p2c][p1c]
-  return memo unless memo.nil?
-
+play = lambda do |p1, p2|
   round = 1
-  ggame += 1
-  game = ggame
-  hand_history = []
+  game += 1
+  hand_history = Hash.new { |h,k| h[k] = Hash.new }
 
-  dputs "\n=== Game #{game} ===\n\n"
-  dputs "Player 1's deck: #{p1c.inspect}"
-  dputs "Player 2's deck: #{p2c.inspect}"
+  winner = nil
 
-  until p1c.empty? || p2c.empty?
-    dputs "\n-- Round #{round} (Game #{game}) --"
+  debug "=== Game #{game} ===\n"
 
-    if igp_rule.call(hand_history)
-      dputs "Player 1 wins!"
-      @memo[p1c][p2c] = :p1
-      @memo[p2c][p1c] = :p1
-      return :p1
-    end
+  until p1.cards.empty? || p2.cards.empty?
+    debug "\n-- Round #{round} (Game #{game}) --"
 
-    hand_history.push [p1c.clone, p2c.clone]
+    return p1 unless hand_history[p1.cards][p2.cards].nil?
 
-    dputs "Player 1's deck: #{p1c.inspect}"
-    dputs "Player 2's deck: #{p2c.inspect}"
+    hand_history[p1.cards][p2.cards] = true
 
-    p1_pick = p1c.shift
-    p2_pick = p2c.shift
+    debug "Player 1's deck: #{p1.deck}"
+    debug "Player 2's deck: #{p2.deck}"
 
-    dputs "Player 1 plays: #{p1_pick}"
-    dputs "Player 2 plays: #{p2_pick}"
+    p1_pick = p1.pick
+    p2_pick = p2.pick
 
-    if p1c.count >= p1_pick && p2c.count >= p2_pick
-      dputs "Playing a sub-game to determine the winner..."
+    debug "Player 1 plays: #{p1_pick}"
+    debug "Player 2 plays: #{p2_pick}"
 
-      p1cc = p1c[0, p1_pick]
-      p2cc = p2c[0, p2_pick]
+    if p1.cards.count >= p1_pick && p2.cards.count >= p2_pick
+      debug "Playing a sub-game to determine the winner...\n\n"
 
-      if play.call(p1cc, p2cc) == :p1
-        @memo[p1cc][p2cc] = :p1
-        @memo[p2cc][p1cc] = :p1
-        dputs "Player 1 wins round #{round} of game #{game}!"
-        p1c.push p1_pick
-        p1c.push p2_pick
+      winner = play.call(
+        p1.with_new_cards(p1_pick),
+        p2.with_new_cards(p2_pick)
+      )
+
+      if winner == p1
+        p1.cards << p1_pick << p2_pick
       else
-        @memo[p1cc][p2cc] = :p2
-        @memo[p2cc][p1cc] = :p2
-        dputs "Player 2 wins round #{round} of game #{game}!"
-        p2c.push p2_pick
-        p2c.push p1_pick
+        p2.cards << p2_pick << p1_pick
       end
     else
       if p1_pick > p2_pick
-        @memo[p1c][p2c] = :p1
-        @memo[p2c][p1c] = :p1
-        dputs "Player 1 wins round #{round} of game #{game}!"
-        p1c.push p1_pick
-        p1c.push p2_pick
+        p1.cards << p1_pick << p2_pick
       else
-        @memo[p1c][p2c] = :p2
-        @memo[p2c][p1c] = :p2
-        dputs "Player 2 wins round #{round} of game #{game}!"
-        p2c.push p2_pick
-        p2c.push p1_pick
+        p2.cards << p2_pick << p1_pick
       end
+
+      debug "Player #{winner} wins round #{round} of game #{game}!"
     end
 
     round += 1
   end
 
-  if p1c.empty?
-    dputs "The winner of game #{game} is player 2!"
-    puts "hand: #{p2c.inspect}\nscore: #{calc_score.call(p2c)}, hhsize: #{hand_history.size}, memosize: #{@memo.size}"
-    :p2
-  else
-    dputs "The winner of game #{game} is player 1!"
-    puts "hand: #{p1c.inspect}\nscore: #{calc_score.call(p1c)}, hhsize: #{hand_history.size}, memosize: #{@memo.size}"
-    :p1
-  end
+  debug "The winner of game #{game} is player #{winner}!"
+
+  p1.cards.empty? ? p2 : p1
 end
 
-p play.call(p1_cards, p2_cards)
+winner = play.call(p1, p2)
+
+debug "\n\n== Post-game results =="
+debug "Player 1's deck: #{p1.deck}"
+debug "Player 2's deck: #{p2.deck}"
+
+score = winner.cards.each_with_index.reduce(0) do |memo, (card, index)|
+  memo += card * (winner.cards.length - index)
+end
+
+puts "\nThe winning player's score: #{score}"
